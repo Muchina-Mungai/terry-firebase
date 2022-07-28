843c6d0d2d820d44b7fbcf2c18c163c5b62a6431
Muchina-Mungai <muchinahmungai@gmail.com>
2022-07-28 17:36:30 +0300

    Changes have been pushed

--- src/server.js
+++ src/server.js
@@ -61,7 +61,7 @@ dotenv.config();
       
     }
   });
-  const PORT=process.env.PORT;
+  const PORT=process.env.PORT||5000;
   app.listen(PORT,()=>{
     console.log(`App started on port ${PORT}`);
   })
